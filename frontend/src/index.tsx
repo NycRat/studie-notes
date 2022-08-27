2876865bdec07d3fec7fb07c489f96f2ae6a5a5c
Eric Xiao <ericxiao0000@gmail.com>
2022-08-27 16:37:15 -0700

    Frontend: added routing, navbar, renamed features to components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>
 );
